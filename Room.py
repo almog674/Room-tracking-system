c50e00992cdf9c520828a8c05c6417bda7e75ec5
Meir Shaish <almogmaimon674@gmail.com>
2022-01-14 17:56:27 +0200

    add yellow

--- Room.py
+++ Room.py
@@ -7,17 +7,16 @@ from global_vars import Global_vars
 
 
 class Room:
-    def __init__(self, width, height, pos,  number, key, screen):
+    def __init__(self, width, height, pos, number, key, screen):
         self.width = width
         self.height = height
         self.pos = pos
         self.number = number
         self.key = key
         self.screen = screen
-        print(key)
-        self.button = Button(key)
 
         self.color = (0, 255, 0)
+        self.state = 0
 
         self.calling = False
         self.font_size = 32
@@ -35,8 +34,10 @@ class Room:
         self.screen.blit(label, (x, self.pos[1] + 20))
 
         # If active change color to red
-        if self.calling:
+        if self.state == 1:
             self.color = (255, 0, 0)
+        elif self.state == 2:
+            self.color = (255, 255, 0)
         else:
             self.color = (0, 255, 0)
 
@@ -46,7 +47,16 @@ class Room:
     def check_for_click(self):
         if Global_vars.IS_KEYDOWN:
             if Global_vars.KEYDOWN_UNICODE == self.key:
-                if not self.calling:
+                if self.state == 0:
                     Global_vars.RING_SOUND.play()
-                self.calling = not self.calling
+                    self.state += 1
+                elif self.state == 1:
+                    Global_vars.RING_SOUND.play()
+                    self.state = 2
+                else:
+                    self.state = 0
+            if Global_vars.KEYDOWN_UNICODE.isnumeric():
+                if int(Global_vars.KEYDOWN_UNICODE) == self.number:
+                    self.state = 0
+                    print('clicked')
                 
